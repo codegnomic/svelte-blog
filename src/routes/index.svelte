eb497bf5f2dee7f68b74c17d8f254fbc1430a296
Code Gnomic <codegnomic@proton.me>
2022-08-17 15:16:37 -0400

    import typewriter

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,10 @@
 <script>
 	import { faker } from '@faker-js/faker';
+	import Typewriter from '$comp/Typewriter.svelte';
 </script>
+
+<div class="p-8">
+	<Typewriter classes="text-xl" txt={'frontend development'} speed={100} />
+</div>
 
 {faker.lorem.paragraphs(20, '\n\n')}
