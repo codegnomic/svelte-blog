ca296479834b897180d50a630508fb792ead5cf1
Code Gnomic <codegnomic@proton.me>
2022-08-17 15:15:56 -0400

    initial

---
+++ src/lib/components/Typewriter.svelte
@@ -0,0 +1,35 @@
+<script type="ts">
+	import { onMount } from 'svelte';
+
+	let i = 0;
+	export let txt: String;
+	let typed = '';
+	let lastTyped = '';
+	export let speed = 50;
+	export let classes: string;
+
+	onMount(async () => typeWriter());
+
+	function typeWriter() {
+		if (i < txt.length) {
+			typed += lastTyped;
+			lastTyped = txt.charAt(i);
+			i++;
+			setTimeout(typeWriter, speed);
+		}
+	}
+</script>
+
+<span class={classes}>{typed}</span><span class={`blink ${classes}`}>{lastTyped}</span>
+
+<style>
+	.blink {
+		animation: blink 0.5s step-end infinite alternate;
+	}
+
+	@keyframes blink {
+		50% {
+			background-color: red;
+		}
+	}
+</style>
