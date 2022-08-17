1a85d1e9c18618ff80f9a567f149faf22c652289
Code Gnomic <codegnomic@proton.me>
2022-08-17 15:16:16 -0400

    no idea

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,7 +5,7 @@
 
 <main class="grid sm:grid-cols-[auto_240px]">
 	<Nav />
-	<div class="grow p-4 bg-yellow-200">
+	<div class="grow p-4 bg-yellow-200 max-w-3xl mx-auto">
 		<slot />
 	</div>
 </main>
